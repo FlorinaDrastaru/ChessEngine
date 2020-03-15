85832f94fbe017facd5db57a98f62b23e452ba3d
FlorinaDrastaru <58330299+FlorinaDrastaru@users.noreply.github.com>
2020-03-15 12:38:10 +0200

    Update ChessGame.java

--- src/com/game/ChessGame.java
+++ src/com/game/ChessGame.java
@@ -30,7 +30,8 @@ public class ChessGame {
         pos.put(6, "g");
         pos.put(7, "h");
     }
-
+    
+    // create a Singleton in order to have a single instance of the game
     public static ChessGame getInstance() {
         if (instance == null) {
             instance = new ChessGame();
@@ -107,12 +108,14 @@ public class ChessGame {
         }
         if (!moved) new Resign().executeCommand();
     }*/
-
+    
+    // execute a move on the table
     public void move() {
         ChessBoard board = ChessBoard.getInstance();
         boolean moved = false;
         for (int i = 7; i > 0; --i) {
             for (int j = 0; j < 8; ++j) {
+                // search for a valid move and send it to the xboard
                 if (board.verifyPosition(new Position(i, j)) && board.getChessPiece(new Position(i, j)).idx == 0
                         && board.getChessPiece(new Position(i, j)).getColour().equals(ChessGame.getInstance().getColour())) {
                     Pawn pawn = (Pawn) board.getChessPiece(new Position(i, j));
@@ -124,7 +127,8 @@ public class ChessGame {
                                 + (ln + ChessGame.getInstance().getSign()) + "\n");
                         moved = true;
                         break;
-                    } else { // daca n are loc sa mearga in fata verifica daca poate sa manance
+                    } else {
+                        // verify if the piece can eliminate another piece
                         pawn.eatOpponent();
                         if (pawn.getPosition().getRow() != i) {
                             int ln = i + 1;
@@ -141,9 +145,9 @@ public class ChessGame {
         }
         if (!moved) new Resign().executeCommand();
     }
-
+    // converts the move received from xboard in a Position type object
+    // modify the position of the opponent on the board
     public void oppMove(String opponentMove) {
-        // converteste alea in locatie si il pune acolo si il sterge de unde era easy
         ChessBoard board = ChessBoard.getInstance();
 
         String sourceCol = opponentMove.substring(0,1);
@@ -153,15 +157,15 @@ public class ChessGame {
 
         int sourceColumn = -1;
         int destColumn = -1;
-
+        
         for (Map.Entry<Integer, String> p : pos.entrySet()) {
             if (p.getValue().equals(sourceCol)) sourceColumn = p.getKey();
             if (p.getValue().equals(destCol)) destColumn = p.getKey();
         }
         Position source = new Position(--sourceRow, sourceColumn);
         Position dest = new Position(--destRow, destColumn);
-
-
+        
+        // verify if the move of the opponent is valid
         if (source.isValidPosition() && dest.isValidPosition() &&
                 board.verifyPosition(source)) {
             ChessPiece chessPiece = board.getChessPiece(source);
