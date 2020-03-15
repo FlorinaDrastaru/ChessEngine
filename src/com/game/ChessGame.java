61b0a460fc6e736993f4096371fae6d2eac28bd4
irinandrvrabie <irinaandreeavrabie@gmail.com>
2020-03-15 23:43:41 +0200

    eat first

--- src/com/game/ChessGame.java
+++ src/com/game/ChessGame.java
@@ -7,7 +7,6 @@ import com.pieces.ChessPiece;
 import com.pieces.Pawn;
 import com.pieces.TeamColour;
 
-import javax.print.DocFlavor;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -37,6 +36,71 @@ public class ChessGame {
             instance = new ChessGame();
         }
         return instance;
+    }
+
+    // execute a move on the table
+    public void move() {
+        ChessBoard board = ChessBoard.getInstance();
+        boolean moved = false;
+        for (int i = 7; i > 0; --i) {
+            for (int j = 0; j < 8; ++j) {
+                // search for a valid move and send it to the xboard
+                if (board.verifyPosition(new Position(i, j)) && board.getChessPiece(new Position(i, j)).idx == 0
+                        && board.getChessPiece(new Position(i, j)).getColour().equals(ChessGame.getInstance().getColour())) {
+                    Pawn pawn = (Pawn) board.getChessPiece(new Position(i, j));
+                    pawn.eatOpponent();
+                    if (pawn.getPosition().getRow() != i) {
+                        int ln = i + 1;
+                        int col = pawn.getPosition().getColumn();
+                        System.out.print("move " + pos.get(j) + ln + pos.get(col)
+                                + (ln + ChessGame.getInstance().getSign()) + "\n");
+                        moved = true;
+                        break;
+                    }
+                    if (new Position(i + ChessGame.getInstance().getSign(), j).isValidPosition()
+                            && !board.verifyPosition(new Position(i + ChessGame.getInstance().getSign(), j))) {
+                        pawn.move(new Position(i + ChessGame.getInstance().getSign(), j));
+                        int ln = i + 1;
+                        System.out.print("move " + pos.get(j) + ln + pos.get(j)
+                                + (ln + ChessGame.getInstance().getSign()) + "\n");
+                        moved = true;
+                        break;
+                    }
+                }
+            }
+            if (moved) break;
+        }
+        if (!moved) new Resign().executeCommand();
+    }
+
+    // converts the move received from xboard in a Position type object
+    // modify the position of the opponent on the board
+    public void oppMove(String opponentMove) {
+        ChessBoard board = ChessBoard.getInstance();
+
+        String sourceCol = opponentMove.substring(0,1);
+        int sourceRow = Integer.parseInt(opponentMove.substring(1,2));
+        String destCol = opponentMove.substring(2,3);
+        int destRow = Integer.parseInt(opponentMove.substring(3));
+
+        int sourceColumn = -1;
+        int destColumn = -1;
+        
+        for (Map.Entry<Integer, String> p : pos.entrySet()) {
+            if (p.getValue().equals(sourceCol)) sourceColumn = p.getKey();
+            if (p.getValue().equals(destCol)) destColumn = p.getKey();
+        }
+        Position source = new Position(--sourceRow, sourceColumn);
+        Position dest = new Position(--destRow, destColumn);
+        
+        // verify if the move of the opponent is valid
+        if (source.isValidPosition() && dest.isValidPosition() &&
+                board.verifyPosition(source)) {
+            ChessPiece chessPiece = board.getChessPiece(source);
+            board.takeOutChessPiece(source);
+            board.putChessPiece(chessPiece, dest);
+            chessPiece.setPosition(dest);
+        }
     }
 
     public TeamColour getColour() {
@@ -73,105 +137,5 @@ public class ChessGame {
 
     public Map<Integer, String> getPos() {
         return pos;
-    }
-
-    /*public void move2() {  // mutarea noastra, in if verifica si daca nu cumva a fost mancat si e
-        // piesa alba acolo ca incerca sa o faca indiferent.
-        // am mutat move la joc ca avea mai mult sens aici ca e cum jucam noi nu i comanda primita gen
-        //cum crezi si tu acuma
-        ChessBoard board = ChessBoard.getInstance();
-        boolean moved = false;
-        for (int i = 6; i > 0; --i) {
-            for (int j = 0; j < 8; ++j) {
-                if (board.verifyPosition(new Position(i, j)) && board.getChessPiece(new Position(i, j)).idx == 0
-                        && board.getChessPiece(new Position(i, j)).getColour().equals(TeamColour.Black)) {
-                    Pawn pawn = (Pawn) board.getChessPiece(new Position(i, j));
-                    if (!board.verifyPosition(new Position(i-1, j))) {
-                        pawn.move(new Position(i-1, j));
-                        int ln = i + 1;
-                        System.out.print("move " + pos.get(j) + ln + pos.get(j) + i + "\n");
-                        moved = true;
-                        break;
-                    } else { // daca n are loc sa mearga in fata verifica daca poate sa manance
-                        pawn.eatOpponent();
-                        if (pawn.getPosition().getRow() != i) {
-                            int ln = i + 1;
-                            int col = pawn.getPosition().getColumn();
-                            System.out.print("move " + pos.get(j) + ln + pos.get(col) + i + "\n");
-                            moved = true;
-                            break;
-                        }
-                    }
-                }
-            }
-            if (moved) break;
-        }
-        if (!moved) new Resign().executeCommand();
-    }*/
-    
-    // execute a move on the table
-    public void move() {
-        ChessBoard board = ChessBoard.getInstance();
-        boolean moved = false;
-        for (int i = 7; i > 0; --i) {
-            for (int j = 0; j < 8; ++j) {
-                // search for a valid move and send it to the xboard
-                if (board.verifyPosition(new Position(i, j)) && board.getChessPiece(new Position(i, j)).idx == 0
-                        && board.getChessPiece(new Position(i, j)).getColour().equals(ChessGame.getInstance().getColour())) {
-                    Pawn pawn = (Pawn) board.getChessPiece(new Position(i, j));
-                    if (new Position(i + ChessGame.getInstance().getSign(), j).isValidPosition()
-                        && !board.verifyPosition(new Position(i + ChessGame.getInstance().getSign(), j))) {
-                        pawn.move(new Position(i + ChessGame.getInstance().getSign(), j));
-                        int ln = i + 1;
-                        System.out.print("move " + pos.get(j) + ln + pos.get(j)
-                                + (ln + ChessGame.getInstance().getSign()) + "\n");
-                        moved = true;
-                        break;
-                    } else {
-                        // verify if the piece can eliminate another piece
-                        pawn.eatOpponent();
-                        if (pawn.getPosition().getRow() != i) {
-                            int ln = i + 1;
-                            int col = pawn.getPosition().getColumn();
-                            System.out.print("move " + pos.get(j) + ln + pos.get(col)
-                                    + (ln + ChessGame.getInstance().getSign()) + "\n");
-                            moved = true;
-                            break;
-                        }
-                    }
-                }
-            }
-            if (moved) break;
-        }
-        if (!moved) new Resign().executeCommand();
-    }
-    // converts the move received from xboard in a Position type object
-    // modify the position of the opponent on the board
-    public void oppMove(String opponentMove) {
-        ChessBoard board = ChessBoard.getInstance();
-
-        String sourceCol = opponentMove.substring(0,1);
-        int sourceRow = Integer.parseInt(opponentMove.substring(1,2));
-        String destCol = opponentMove.substring(2,3);
-        int destRow = Integer.parseInt(opponentMove.substring(3));
-
-        int sourceColumn = -1;
-        int destColumn = -1;
-        
-        for (Map.Entry<Integer, String> p : pos.entrySet()) {
-            if (p.getValue().equals(sourceCol)) sourceColumn = p.getKey();
-            if (p.getValue().equals(destCol)) destColumn = p.getKey();
-        }
-        Position source = new Position(--sourceRow, sourceColumn);
-        Position dest = new Position(--destRow, destColumn);
-        
-        // verify if the move of the opponent is valid
-        if (source.isValidPosition() && dest.isValidPosition() &&
-                board.verifyPosition(source)) {
-            ChessPiece chessPiece = board.getChessPiece(source);
-            board.takeOutChessPiece(source);
-            board.putChessPiece(chessPiece, dest);
-            chessPiece.setPosition(dest);
-        }
     }
 }
