ffcbd69df8b5033742b74b073c03163eda22b584
FlorinaDrastaru <58330299+FlorinaDrastaru@users.noreply.github.com>
2020-03-15 12:43:42 +0200

    Update Black.java

--- src/com/commands/Black.java
+++ src/com/commands/Black.java
@@ -3,12 +3,13 @@ package com.commands;
 import com.game.ChessGame;
 import com.pieces.TeamColour;
 
+// reset the colour of the engine to black 
+// and the direction of moving on the board 
 public class Black implements Command {
     @Override
     public void executeCommand() {
         ChessGame.getInstance().setWhite(false);
         ChessGame.getInstance().setSign(-1);
         ChessGame.getInstance().setColour(TeamColour.Black);
-        //ChessGame.getInstance().move();
     }
 }
