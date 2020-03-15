ca38d74a3e2f3e762dcdfddd3cdb75bbb6fca8ce
FlorinaDrastaru <58330299+FlorinaDrastaru@users.noreply.github.com>
2020-03-15 12:40:10 +0200

    Update NewGame.java

--- src/com/commands/NewGame.java
+++ src/com/commands/NewGame.java
@@ -4,12 +4,16 @@ import com.board.ChessBoard;
 import com.game.ChessGame;
 import com.pieces.TeamColour;
 
+// initiate a new game
 public class NewGame implements Command {
     @Override
     public void executeCommand() {
         ChessBoard chessBoard = ChessBoard.getInstance();
+        // reinitiate the board
         chessBoard.initiateBoard();
+        // set the default colour of the game
         ChessGame.getInstance().setColour(TeamColour.Black);
+        // set the default way of moving on the table
         ChessGame.getInstance().setSign(-1);
     }
 }
